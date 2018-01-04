4a37fcf5c2c60ed7898f4c21c0e53a39fdeb680d
Marcin Piechota <piechota.marcin@gmail.com>
2018-01-04 10:43:22 +0100

    update

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
-	$('.turn-on-overlay').hide();
 	$('.overlay').click(function(){
 		$(this).hide();
 		$('.turn-on-overlay').show();
-	});
-	$('.turn-on-overlay').click(function(){
-		$(this).hide();
-		$('.overlay').show();
 	});
 });
